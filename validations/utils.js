d11fb40d512ab8ca15922737cc760207187085ec
carlosbecker2077 <carlos.becker663@gmail.com>
2022-07-04 14:28:40 -0300

    finished type and math tests

--- validations/utils.js
+++ validations/utils.js
@@ -12,44 +12,47 @@ function eTexto(s1) {
     if (typeof s1 === 'string'){
         if (/[a-z]/i.test(s1) && !/[0-9]/.test(s1) && !lista.includes(s1)){
             return true
-        } else return false
-    }else return false
+        } throw new TypeError('Invalid data type')          //else return false
+    } throw new TypeError('Invalid data type')              //else return false
 
 }
 
 const sum = (a, b) => {
     if(eNumero(a) && eNumero(b)){
         return Number(a) + Number(b)
-    } else {
-        return 'Operação inválida'
-    }
+    } throw new TypeError('Invalid data type')
 }
 
 const sub = (a, b) => {
     if(eNumero(a) && eNumero(b)){
         return a - b
-    } else {
-        return 'Operação inválida'
-    }
+    } throw new TypeError('Invalid data type')
 }
 
 const mult = (a,b) => {
     if(eNumero(a) && eNumero(b)){
         return a * b
-    } else {
-        return 'Operação inválida'
-    }
+    } throw new TypeError('Invalid data type')
 }
 
 const divi = (a,b) => {
     if(eNumero(a) && eNumero(b)){
         if (b > 0){
             return a / b
-        } else return 'Operação inválida: divisão por zero'
-    } else {
-        return 'Operação inválida'
+        } throw new Error('Invalid operation: division by zero')
+    } throw new TypeError('Invalid data type')
+}
+
+const calcula = (op, a, b) => {
+    switch (op) {
+        case '+': sum(a, b)
+        case '-': sub(a, b)
+        case '*': mult(a, b)
+        case '/': divi(a, b)
     }
 }
 
 
-module.exports = { eNumero, eTexto, sum, sub, mult, divi}
+
+
+module.exports = { eNumero, eTexto, sum, sub, mult, divi, calcula}
--- validations/utils.test.js
+++ validations/utils.test.js
@@ -2,57 +2,298 @@ const { eTexto, eNumero, sum, sub, mult, divi } = require("./utils")
 require('jest')
 
 describe('type functions', () => {
-    it('test if it is a number only', () => {
-        expect(eNumero('5555')).toBe(true)
+    it('should not be able to accept a letter', () => {
+        const t = () => {
+            eNumero('5555b')
+        }
+        expect(t).toThrow(TypeError)
+        expect(t).toThrow("Invalid data type")
     })
 
     it('should not be able to accept a empty string', () => {
         const t = () => {
             eNumero(' ')
           };
-          expect(t).toThrow(TypeError);
-          expect(t).toThrow("Invalid data type");
-    })
-
-    it('test if it is a text only', () => {
-        expect(eTexto('s')).toBe(true)
-    })
-
-    it('should not be able to accept a number in a string', () => {
-        expect(eTexto('dhjuiwhaiusd5')).toBe(false)
-    })
-
-    it('should not be able to accept a number', () => {
-        expect(eTexto(6)).toBe(false)
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
     })
 
     it('should not be able to accept a symbol', () => {
-        expect(eTexto('$')).toBe(false)
+        const t = () => {
+            eNumero('.')
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+        
     })
 
-    it('should not be able to accept a space', () => {
-        expect(eTexto(' ')).toBe(false)
+    it('should not be able to accept undefined type', () => {
+        const t = () => {
+            eNumero(undefined)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
     })
 
-    it('should not be able to accept empty', () => {
-        expect(eTexto('')).toBe(false)
+    it('should not be able to accept undefined string', () => {
+        const t = () => {
+            eNumero('undefined')
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+    })
+
+    it('should not be able to accept a NaN type', () => {
+        const t = () => {
+            eNumero(NaN)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+    })
+
+    it('should be able to accept a string made only of numbers', () => {
+        expect(eNumero('3454545656567878787654543')).toBe(true)
+    })
+
+    it('should be able to accept a number', () => {
+        expect(eNumero(2)).toBe(true)
+          
+    })
+
+    it('should be able to accept a long number', () => {
+        expect(eNumero(100000000000000000000)).toBe(true) //max 20 zeroes
+          
+    })
+
+    it('should be able to accept a floating number string', () => {
+        expect(eNumero('34.5')).toBe(true)
+          
+    })
+
+    it('should be able to accept a floating number', () => {
+        expect(eNumero(34.5)).toBe(true)
+          
+    })
+
+    it('should not be able to accept string with a number', () => {
+        const t = () => {
+            eTexto('abcde 5555')
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+        
+    })
+
+    it('should not be able to accept symbols only', () => {
+        const t = () => {
+            eTexto('!@#$%¨&*()')
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+        
+    })
+
+    it('should not be able to accept null, undefined, NaN and Infinity strings and types', () => {
+        const t = () => {
+            eTexto('null')
+            eTexto('undefined')
+            eTexto('NaN')
+            eTexto('Infinity')
+            eTexto(null)
+            eTexto(undefined)
+            eTexto(NaN)
+            eTexto(Infinity)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+        
+    })
+
+    it("should be able to accept capital letters", () => {
+        expect(eTexto('JohnBergman')).toBe(true)       
+    })
+    
+    it("should be able to accept symbols with some text", () => {
+        expect(eTexto('meu nome, é: John Bergman.!@#$%¨&*()_+')).toBe(true)
+        expect(eTexto('s#')).toBe(true)
+        expect(eTexto('#s')).toBe(true)
+        
     })
 
 })
 describe('math functions', () => {
-    it('sums 2 numbers', () =>{
-        expect(sum(1,2)).toBe(3)
+    it('should not be able to sum a string', () =>{
+        const t = () => {
+            sum('a', 2)
+            sum('#', 2)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
     })
 
-    it('subtracts 2 numbers', () => {
-        expect(sub(5,3)).toBe(2)
+    it('should not be able to sum a space', () =>{
+        const t = () => {
+            sum(' ', 2)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
     })
 
-    it('multiplies 2 numbers', () => {
-        expect(mult(5,3)).toBe(15)
+    it('should not be able to sum a nan, null ...', () =>{
+        const t = () => {
+            sum('null')
+            sum('undefined')
+            sum('NaN')
+            sum('Infinity')
+            sum(null)
+            sum(undefined)
+            sum(NaN)
+            sum(Infinity)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+    })
+    
+    it('should be able to sum a floating number string', () => {
+        expect(sum('34.5', 2)).toBe(36.5)
+          
     })
 
-    it('divide 2 numbers', () => {
-        expect(divi(6,2)).toBe(3)
+    it('should be able to sum a floating number', () => {
+        expect(sum('34.5', 2)).toBe(36.5)    
+    })
+
+    it('should not be able to sub a string', () =>{
+        const t = () => {
+            sub('a', 2)
+            sub('#', 2)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+    })
+
+    it('should not be able to sub a space', () =>{
+        const t = () => {
+            sub(' ', 2)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+    })
+
+    it('should not be able to sub a nan, null ...', () =>{
+        const t = () => {
+            sub('null')
+            sub('undefined')
+            sub('NaN')
+            sub('Infinity')
+            sub(null)
+            sub(undefined)
+            sub(NaN)
+            sub(Infinity)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+    })
+    
+    it('should be able to sub a floating number string', () => {
+        expect(sub('34.5', 2)).toBe(32.5)
+          
+    })
+
+    it('should be able to sub a floating number', () => {
+        expect(sub(34.5, 2)).toBe(32.5)
+          
+    })
+
+    it('should not be able to mult a string', () =>{
+        const t = () => {
+            mult('a', 2)
+            mult('#', 2)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+    })
+
+    it('should not be able to mult a space', () =>{
+        const t = () => {
+            mult(' ', 2)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+    })
+
+    it('should not be able to mult a nan, null ...', () =>{
+        const t = () => {
+            mult('null')
+            mult('undefined')
+            mult('NaN')
+            mult('Infinity')
+            mult(null)
+            mult(undefined)
+            mult(NaN)
+            mult(Infinity)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+    })
+    
+    it('should be able to mult a floating number string', () => {
+        expect(mult('34.5', 2)).toBe(69)
+          
+    })
+
+    it('should be able to mult a floating number', () => {
+        expect(mult(34.5, 2)).toBe(69)
+          
+    })
+
+    it('should not be able to divi a string', () =>{
+        const t = () => {
+            divi('a', 2)
+            divi('#', 2)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+    })
+
+    it('should not be able to divi a space', () =>{
+        const t = () => {
+            divi(' ', 2)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+    })
+
+    it('should not be able to divi a nan, null ...', () =>{
+        const t = () => {
+            divi('null')
+            divi('undefined')
+            divi('NaN')
+            divi('Infinity')
+            divi(null)
+            divi(undefined)
+            divi(NaN)
+            divi(Infinity)
+          };
+          expect(t).toThrow(TypeError)
+          expect(t).toThrow("Invalid data type")
+    })
+    
+    it('should be able to divi a floating number string', () => {
+        expect(divi('34.5', 2)).toBe(17.25)
+          
+    })
+
+    it('should be able to divi a floating number', () => {
+        expect(divi(34.5, 2)).toBe(17.25)
+          
+    })
+
+    it('should not be able to divide by zero', () =>{
+        const t = () => {
+            divi(10, 0)
+          };
+          expect(t).toThrow(Error)
+          expect(t).toThrow("Invalid operation: division by zero")
     })
 })
